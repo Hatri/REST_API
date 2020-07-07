cd96ae6e6ba596c50eb288c8ea1df2c80e5fd214
Hatri <18404171+Hatri@users.noreply.github.com>
2020-07-07 17:48:24 +0200

    Added Routes

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -1,4 +1,16 @@
 const express = require('express')
 const router = express.Router()
 
+/* Getting All */
+router.get('/', (req, res) => {
+
+})
+/* Getting One */
+
+/* Creating One */
+
+/* Updating One  */
+
+/* Deleting One  */
+
 module.exports = router
