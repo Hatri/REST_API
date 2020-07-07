50ee11ef563a592c98f3587dad5b9b8ca61df373
Hatri <18404171+Hatri@users.noreply.github.com>
2020-07-07 17:36:42 +0200

    Routes

--- server.js
+++ server.js
@@ -9,4 +9,8 @@ const db = mongoose.connection
 db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('Connected to Database'))
 
+app.use(express.json())
+
+const subscribersRouter = require('./routes/subscribers')
+
 app.listen(3000, () => console.log('Server Started'))
