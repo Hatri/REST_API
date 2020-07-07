0f074cfc6a7905f7646b6241011c26bd343ceb9a
Hatri <18404171+Hatri@users.noreply.github.com>
2020-07-07 17:52:18 +0200

    Unit test

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -3,14 +3,24 @@ const router = express.Router()
 
 /* Getting All */
 router.get('/', (req, res) => {
-
+    res.send('Test')
 })
 /* Getting One */
-
+router.get('/:id', (req, res) => {
+    res.send(req.params.id)
+})
 /* Creating One */
+router.post('/', (req, res) => {
 
+})
 /* Updating One  */
+router.patch('/', (req, res) => {
+
+})
 
 /* Deleting One  */
+router.delete('/:id', (req, res) => {
+
+})
 
 module.exports = router
